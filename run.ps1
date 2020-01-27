b3ab4b37324502992985391ea2651da723aa8110
Michael Quevillon <m.quevil@gmail.com>
2020-01-26 20:04:29 -0500

    Update to v2.43.0

--- run.ps1
+++ run.ps1
@@ -1,7 +1,7 @@
 # Download newest version and get version number
 $pkg = "Caprine"
 $lpkg = "$pkg".toLower()
-$version = "2.42.1"
+$version = "2.43.0"
 $fname = "$pkg-Setup-$version.exe"
 
 #Update nuspec with version number
