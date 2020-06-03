cc35b1152301c14cf951ca3f539cf5e2ad1bc8f2
Michael Quevillon <m.quevil@gmail.com>
2020-06-03 11:08:31 -0400

    Update to v2.47.0

--- run.ps1
+++ run.ps1
@@ -1,7 +1,7 @@
 # Download newest version and get version number
 $pkg = "Caprine"
 $lpkg = "$pkg".toLower()
-$version = "2.46.0"
+$version = "2.47.0"
 $fname = "$pkg-Setup-$version.exe"
 
 #Update nuspec with version number
