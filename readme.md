8b00984a89d16d4a405c8e30c1e99470bfb41606
Michael Quevillon <mquevill@nd.edu>
2021-09-15 11:18:09 -0400

    Get newest version automatically

---
+++ readme.md
@@ -0,0 +1,17 @@
+# Chocolatey-Caprine
+Source files of Chocolatey package for Caprine
+
+Automatically downloads the newest version of Caprine,
+packages it into a Chocolatey package,
+and pushes it to the Chocolatey website.
+
+Simply run with
+```powershell
+.\run.ps1
+```
+
+_NOTE:_ Requires [powershell-yaml](https://github.com/cloudbase/powershell-yaml) module:
+from the [PowerShell Gallery](https://www.powershellgallery.com/)
+```powershell
+Install-Module powershell-yaml
+```
--- run.ps1
+++ run.ps1
@@ -1,22 +1,30 @@
-# Download newest version and get version number
-$pkg = "Caprine"
-$lpkg = "$pkg".toLower()
-$version = "2.54.1"
-$fname = "$pkg-Setup-$version.exe"
+# Get information from GitHub repository
+$repo = "sindresorhus/caprine"
+$url =  "https://api.github.com/repos/$repo/releases"
+$tag = (Invoke-WebRequest $url | ConvertFrom-Json)[0].tag_name
+$version = $tag -replace 'v','' # assumes "vX.Y.Z" format
+$download = "https://github.com/$repo/releases/download/$tag"
 
-#Update nuspec with version number
+# Download latest.yml and extract information
+$fyml = "latest.yml"
+$info = (Invoke-WebRequest "$download/$fyml" | ConvertFrom-Yaml)
+$fname = $info.path
+$pkg = $fname.split('-')[0] # assumes "Pkg-xxx.exe" format
+$lpkg = $pkg.toLower()
+$csum = $info.sha512
+
+# Update nuspec with version number
 (Get-Content .\$pkg.nuspec.skel).replace('VERVERVER', $version) | Set-Content .\$pkg.nuspec
 
-#Update tools/VERIFICATION.txt with checksum
-$csum = (checksum -t=sha512 .\$fname)
+# Update tools/VERIFICATION.txt with checksum
 (Get-Content .\tools\VERIFICATION.txt.skel).replace('NUMNUMNUM', $csum) | Set-Content .\tools\VERIFICATION.txt
 
-#Update tools/chocolateyinstall.ps1 with filename and checksum
+# Update tools/chocolateyinstall.ps1 with filename and checksum
 (Get-Content .\tools\chocolateyinstall.ps1.skel).replace('NUMNUMNUM', $csum).replace('VERVERVER', $version) | Set-Content .\tools\chocolateyinstall.ps1
 
-#Package and push to Chocolatey
+# Package and push to Chocolatey
 cpack .\$pkg.nuspec
 cpush .\$lpkg.$version.nupkg
 
-#Clean up
+# Clean up
 rm .\$pkg.nuspec, .\tools\VERIFICATION.txt, .\tools\chocolateyinstall.ps1
